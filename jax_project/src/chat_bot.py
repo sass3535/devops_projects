bca2997e856bd47a0175bb1c99ca0415b20af967
msass <mattsassoon@gmail.com>
2026-03-10 14:46:43 -0400

    Modified main to include db.py and added write persistence to the database.

--- jax_project/src/chat_bot.py
+++ jax_project/src/chat_bot.py
@@ -5,7 +5,8 @@ All configuration logic is loaded via a .env file, allowing for user customizati
 import os
 import anthropic
 from dotenv import load_dotenv
-import database
+from src import database
+import time
 
 def load_config():
     
@@ -45,7 +46,9 @@ def get_user_input():
 
 def receive_response(client: anthropic.Anthropic, config: dict, history: list):
     
-    """Send conversation history to the API and return the reply text."""
+    """Stream a response from the Anthropic API."""
+    #Capture start of message
+    start_time = time.perf_counter()
     
     with client.messages.stream(
         model=config["model_name"],
@@ -57,9 +60,22 @@ def receive_response(client: anthropic.Anthropic, config: dict, history: list):
         for text in stream.text_stream:
             print(text, end="", flush=True)
         print("\n")
-        return stream.get_final_message().content[0].text
-
-
+        final_message = stream.get_final_message()
+        
+        #Capture time of message stream
+        end_time = time.perf_counter()
+        
+        #Capture tokens and reply assign to variables
+        reply = final_message.content[0].text
+        input_tokens = final_message.usage.input_tokens
+        output_tokens = final_message.usage.output_tokens
+        
+        #latency calculation
+        latency_ms = (end_time - start_time) * 1000
+        
+        #Return reply response from LLM, input tokens, output tokens, and latency of transaction
+        return (reply, input_tokens, output_tokens, latency_ms)
+    
 def run_chat_bot():
     
     """Run the interactive terminal chat bot loop."""
@@ -74,7 +90,6 @@ def run_chat_bot():
     user_id = database.insert_users(first_name, last_name)
     session_id = database.insert_sessions(user_id, config["model_name"])
     
-    
     print("\nChatbot ready. Type 'quit' or 'exit' to end session.\n")
 
     while True:
@@ -83,11 +98,15 @@ def run_chat_bot():
             break
         if not user_input:
             continue
-
+        
         history.append({"role": "user", "content": user_input})
-        reply = receive_response(client, config, history)
+        reply, input_tokens, output_tokens, latency_ms = receive_response(client, config, history)
+        
+        #Store into databases
+        message_id = database.insert_messages(session_id, user_input, input_tokens)
+        database.insert_responses(message_id, reply, output_tokens, latency_ms)
+        
         history.append({"role": "assistant", "content": reply})
-
 
 if __name__ == "__main__":
     run_chat_bot()
